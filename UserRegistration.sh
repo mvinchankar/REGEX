b4bd261d93653b690be919cf47babe8bc037c3b2
mvinchankar <mvinchankar18@gmail.com>
2019-11-21 09:56:01 +0530

    ADDED validation of mobile number

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -3,9 +3,9 @@ shopt -s extglob
 read -p "Enter word to match :" word
 pat1="^[A-Z][a-z]{3,}"
 pat2="[A-Z][a-z]{3,}$"
-pat6='^[a-zA-Z0-9]([._+]{0,1}[a-zA-Z0-9])*[@]{1}[a-zA-Z]{1,}[.]{1}[a-zA-Z]{2,3}([.]{1}[a-zA-Z]{2}){0,1}$'
-
-if [[ $word =~ $pat6 ]]
+pat3='^[a-zA-Z0-9]([._+]{0,1}[a-zA-Z0-9])*[@]{1}[a-zA-Z]{1,}[.]{1}[a-zA-Z]{2,3}([.]{1}[a-zA-Z]{2}){0,1}$'
+pat4='^[0-9]{2}\s[0-9]{10}'
+if [[ $word =~ $pat4 ]]
 then 
     echo VAlid
 else
