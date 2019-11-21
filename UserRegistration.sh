c8a862111acc72ba70a2661a062bd41cdd2f265c
mvinchankar <mvinchankar18@gmail.com>
2019-11-21 09:48:01 +0530

    ADDED email validation

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -3,7 +3,9 @@ shopt -s extglob
 read -p "Enter word to match :" word
 pat1="^[A-Z][a-z]{3,}"
 pat2="[A-Z][a-z]{3,}$"
-if [[ $word =~ $pat2 ]]
+pat6='^[a-zA-Z0-9]([._+]{0,1}[a-zA-Z0-9])*[@]{1}[a-zA-Z]{1,}[.]{1}[a-zA-Z]{2,3}([.]{1}[a-zA-Z]{2}){0,1}$'
+
+if [[ $word =~ $pat6 ]]
 then 
     echo VAlid
 else
