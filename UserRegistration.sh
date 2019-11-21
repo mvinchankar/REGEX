b59b62e3b470b58d0cedd3d472ef3d0f1a89fc84
mvinchankar <mvinchankar18@gmail.com>
2019-11-21 09:40:45 +0530

    ADDED validation for last name

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -x
 shopt -s extglob
 read -p "Enter word to match :" word
-pat1="^[A-Z]{1}[a-z]{3,}"
-if [[ $word =~ $pat1 ]]
+pat1="^[A-Z][a-z]{3,}"
+pat2="[A-Z][a-z]{3,}$"
+if [[ $word =~ $pat2 ]]
 then 
     echo VAlid
 else
